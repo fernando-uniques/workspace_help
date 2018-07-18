859157b0bae80e0b2f499a38fa796efa41062439
Fernando Rocha <fernandorochaworld@gmail.com>
2018-07-18 11:33:06 -0300

    Script to Change PHP version active in the machine.

---
+++ change_php.sh
@@ -0,0 +1,20 @@
+if [ "$EUID" -ne 0]
+  then echo "Favor, execute este script com sudo"
+  exit
+fi
+
+if [ -z "$1" ]
+        then echo "Informe a versão atual do PHP." 
+        exit
+fi
+
+if [ -z "$2" ]
+        then echo "Informe a versão desejada do PHP." 
+        exit
+fi
+
+a2dismod php$1 ; a2enmod php$2
+update-alternatives --set php /usr/bin/php$2
+service apache2 restart
+
+echo "pronto"
