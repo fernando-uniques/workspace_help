c82555c8f8ad2ae9b0fa8548f99358b705a5037b
fernando <fernandorochaworld@gmail.com>
2019-02-19 08:35:00 -0300

    ter fev 19 08:34:59 -03 2019 - auto_test - no error found

--- gitup.sh
+++ gitup.sh
@@ -1,61 +1,21 @@
-if [ "$EUID" -ne 0]
-  then echo "Please, execute as sudo."
-  exit
-fi
+#if [ "$EUID" -ne 0]
+#  then echo "Please, execute as sudo."
+#  exit
+#fi
 
-if [ -z "$1" ]
-        then echo "Please, specify the host name." 
-        exit
-fi
+cd $ws
+msg="`date` - auto_test - no error found"
 
-mkdir -p /var/www/com
-host_path="/var/www/com/$1"
-host_name="$1.com"
-admin_email="fernandorochaworld@gmail.com"
+# update
+git pull
 
-echo "Creating host $host_name"
+#echo $msg >> ./log.log
+sed -i "1i$msg" ./log.log
+git add log.log
+echo "log.log criado e adicionado";
 
-mkdir -p $host_path
-chown -R $USER:$USER $host_path
-chmod -R 777 $host_path
+git commit -m "$msg"
+echo "commited";
 
-echo "<html>
-  <head>
-    <title>Welcome to $host_name!</title>
-  </head>
-  <body>
-    <h1>Success!</h1>
-    <h2>www.$host_name VirtualHost is working!</h2>
-  </body>
-</html>" > $host_path/index.html
-
-
-echo "<VirtualHost *:80>
-    ServerAdmin $admin_email
-    ServerName $host_name
-    ServerAlias www.$host_name
-    DocumentRoot $host_path
-        <Directory "$host_path">
-#           AllowOverride All
-                Options Indexes FollowSymLinks MultiViews
-                AllowOverride all
-                Order allow,deny
-                allow from all
-        </Directory>
-</VirtualHost>" > /etc/apache2/sites-available/$1.conf
-
-chown www-data /etc/apache2/sites-available/$1.conf
-
-echo "Enabling host"
-a2ensite $1
-service apache2 reload
-
-echo "VirtualHost $1 created"
-
-echo "Creating routes"
-echo "
-127.0.0.1       $host_name
-127.0.0.1       www.$host_name
-" >> /etc/hosts
-
-echo "done"
+git push
+echo "pushed/done";
